4b580def28e2bbe0ef5bf44fb65dcb039f84a7c2
ungoabi <kingkongabi69@gmail.com>
2026-07-21 01:01:01 +0200

    fixed bugs

--- autoloads/audio.gd
+++ autoloads/audio.gd
@@ -3,7 +3,12 @@ extends Node
 
 enum Bus {MASTER, MUSIC, SFX, UI}
 
-const bus_map: Dictionary[Bus, String] = {Bus.MASTER: "Master", Bus.MUSIC: "Music", Bus.SFX: "SFX", Bus.UI: "UI"}
+const bus_map: Dictionary[Bus, StringName] = {
+	Bus.MASTER: &"Master",
+	Bus.MUSIC: &"Music",
+	Bus.SFX: &"SFX",
+	Bus.UI: &"UI"
+}
 
 var current_music_player: AudioStreamPlayer
 
@@ -16,40 +21,39 @@ func play_music(stream: AudioStream) -> void:
 		current_music_player.queue_free()
 	
 	var player = _create_player(stream, Bus.MUSIC)
-	_play_audio(player)
 	current_music_player = player
+	add_child(player)
+	player.play()
 
 
 func stop_music() -> void:
 	if current_music_player:
+		current_music_player.stop()
 		current_music_player.queue_free()
 		current_music_player = null
 
 
 func play_sfx(stream: AudioStream) -> void:
-	if not stream: return
-	
-	var player = _create_player(stream, Bus.SFX)
-	_play_audio(player)
-	player.finished.connect(player.queue_free)
+	_play_one_shot(stream, Bus.SFX, Node.PROCESS_MODE_INHERIT)
 
 
 func play_ui_sound(stream: AudioStream) -> void:
-	if not stream: return
+	_play_one_shot(stream, Bus.UI, Node.PROCESS_MODE_ALWAYS)
+
+
+func _play_one_shot(stream: AudioStream, bus: Bus, mode: Node.ProcessMode) -> void:
+	if not stream:
+		return
 	
-	var player = _create_player(stream, Bus.UI)
-	player.process_mode = Node.PROCESS_MODE_ALWAYS
-	_play_audio(player)
+	var player := _create_player(stream, bus)
+	player.process_mode = mode
 	player.finished.connect(player.queue_free)
+	add_child(player)
+	player.play()
 
 
 func _create_player(stream: AudioStream, bus: Bus) -> AudioStreamPlayer:
-	var player = AudioStreamPlayer.new()
+	var player := AudioStreamPlayer.new()
 	player.stream = stream
-	player.bus = bus_map.get(bus)
+	player.bus = bus_map[bus]
 	return player
-
-
-func _play_audio(player: AudioStreamPlayer) -> void:
-	add_child(player)
-	player.play()
